223ffd926083f149804e3ac885be3d589c7a212e
Neto Kokay <netokokay@gmail.com>
2022-08-09 21:27:57 -0300

    Criando Classe Camisa

---
+++ index.js
@@ -0,0 +1,22 @@
+class Camisa {
+    constructor(tamanho, preco) {
+        this.tamanho = tamanho;
+        this.preco = preco;
+    }
+
+    getTamanho() {
+        return this.tamanho;
+    }
+
+    setTamanho(novoTamanho) {
+        this.tamanho = novoTamanho;
+    }
+
+    getPreco() {
+        return this.getPreco;
+    }
+
+    setPreco(novoPreco) {
+        this.preco = novoPreco;
+    }
+}
